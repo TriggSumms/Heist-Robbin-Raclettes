e51a977eec77d43e6b2e8941db7267a7a467c1f1
Erik Lindstrom <elindstrom42@gmail.com>
2020-08-28 16:01:48 -0500

    Phase six complete

---
+++ Bank.cs
@@ -0,0 +1,9 @@
+namespace heist_robbin_raclettes
+{
+    public class Bank
+    {
+        public int Difficulty {get; set;}
+  
+
+    }
+}
---
+++ Thief.cs
@@ -0,0 +1,11 @@
+namespace heist_robbin_raclettes
+{
+// Template for a Thief
+    public class Thief
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public double CourageLevel { get; set; }
+    }
+}
+
