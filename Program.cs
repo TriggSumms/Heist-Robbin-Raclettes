17457feebbb86212057c7595fc927f460d9b7443
Erik Lindstrom <elindstrom42@gmail.com>
2020-08-28 10:39:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace heist_robbin_raclettes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Heist-Robbin-Raclettes
